cf7729fc2ee0be4e788fae6ff096287496a5eef8
SungJae Jung <dtc13579@gmail.com>
2025-01-17 16:17:02 +0900

    Merge : Feature/init 21 (#22)
    * Init: Frontend React 세팅 (#21)
    - React 사용을 위한 기본 세팅 추가 (Bootstrap 템플릿 추가)
    
    * Feat: Proxy 설정 추가 (#21)
    - CORS 문제 방지를 위한 Frontend 프록시 Config 추가

---
+++ frontend/src/components/product/ProductStyle1.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const ProductStyle1 = () => {
+    return (<div className="col mb-5">
+        <div className="card h-100"> {/* Product image */} <img className="card-img-top"
+                                                                src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
+                                                                alt="Product"/> {/* Product details */}
+            <div className="card-body p-4">
+                <div className="text-center"> {/* Product name */} <h5 className="fw-bolder">Fancy
+                    Product</h5> {/* Product price */} $40.00 - $80.00
+                </div>
+            </div>
+            {/* Product actions */}
+            <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">View options</a></div>
+            </div>
+        </div>
+    </div>);
+};
+export default ProductStyle1;
