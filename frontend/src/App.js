94fc2bfa4f9f7b9729088b7ed98a80953c1a6c03
SungJae Jung <dtc13579@gmail.com>
2025-01-20 17:53:21 +0900

    Merge : Refactor/init-35
    - 라우팅 방식 수정 (HashRouter -> BrowserRouter)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import { useRoutes } from "react-router-dom";
+import { Suspense } from 'react';
 import routes from "./routes/Router";
 
 const App = () => {
-  const routing = useRoutes(routes);
-  return(
-      <div>
-        {routing}
-      </div>
-  );
-}
+    const routing = useRoutes(routes);
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            {routing}
+        </Suspense>
+    );
+};
 
 export default App;
--- frontend/src/routes/Router.js
+++ frontend/src/routes/Router.js
@@ -1,17 +1,12 @@
-import {lazy} from "react";
+import { lazy } from "react";
 import { Navigate } from "react-router-dom";
 
 const Home = lazy(() => import("../pages/home/Home"));
 
-/*****Routes******/
 const AppRoutes = [
-    {
-        path: "/",
-        element: <Home/>,
-        children: [
-            { path: "*", element: <Navigate to="/"/> },
-        ]
-    }
+    { path: "/", element: <Home /> },
+    // { path: "/payment", element: <Payment /> },
+    { path: "*", element: <Navigate to="/" /> }  // 유효하지 않는 URL 입력 시 root URL로 이동
 ];
 
 export default AppRoutes;
