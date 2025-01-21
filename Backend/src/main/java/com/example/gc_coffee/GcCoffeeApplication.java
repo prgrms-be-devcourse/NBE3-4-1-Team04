c45bc9354189cee6074d47334b16d867d68cb0ac
Jinwon <wlsdnjs1120@naver.com>
2025-01-21 22:48:38 +0900

    Merge : feature/admin-39 (#43)
    * feat: 관리자 계정 로그인 기능 구현
    * 관리자 페이지에서 관리자 기능을 사용하기 위해 관리자 로그인 기능 구현

--- Backend/src/main/java/com/example/gc_coffee/GcCoffeeApplication.java
+++ Backend/src/main/java/com/example/gc_coffee/GcCoffeeApplication.java
@@ -13,4 +13,5 @@ public class GcCoffeeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GcCoffeeApplication.class, args);
 	}
+
 }
--- Backend/src/main/java/com/example/gc_coffee/global/exceptions/GlobalExceptionHandler.java
+++ Backend/src/main/java/com/example/gc_coffee/global/exceptions/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package com.example.gc_coffee.global.exceptions;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -27,4 +29,16 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
     }
+
+    // 관리자 로그인 익셉션
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<String> handleUsernameNotFoundException(UsernameNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    }
+    // 관리자 로그인 익셉션
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<String> handleAuthenticationException(AuthenticationException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("인증 실패: " + ex.getMessage());
+    }
+
 }
