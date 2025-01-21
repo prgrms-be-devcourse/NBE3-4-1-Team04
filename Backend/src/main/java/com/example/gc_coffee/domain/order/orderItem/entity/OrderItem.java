65bef0d9059ca30cfea09a7eebff38fe72df42c5
Hyeonsuk <vkdnjdjxor@naver.com>
2025-01-21 12:19:17 +0900

    chore: 설정 파일 및 리소스 파일 수정(#25)

--- Backend/src/main/java/com/example/gc_coffee/domain/order/orderItem/entity/OrderItem.java
+++ Backend/src/main/java/com/example/gc_coffee/domain/order/orderItem/entity/OrderItem.java
@@ -4,18 +4,16 @@ import com.example.gc_coffee.domain.item.entity.Item;
 import com.example.gc_coffee.domain.order.order.entity.Order;
 import com.example.gc_coffee.global.jpa.entity.BaseEntity;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Table(name = "order_items")
 @Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class OrderItem extends BaseEntity {
+public class OrderItems extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "order_item_id")
@@ -29,7 +27,22 @@ public class OrderItem extends BaseEntity {
     @JoinColumn(name = "item_id")
     private Item item;
 
-    private int quantity;
+    private int count;
+
+    //==생성 메서드==//
+    public static OrderItems createOrderItem(Item item, int count) {
+        OrderItems orderItems = new OrderItems();
+        orderItems.setItem(item);
+        orderItems.setCount(count);
+
+        item.removeStock(count);
+        return orderItems;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel() {
+        getItem().addStock(count); //재고 수량을 원상 복구
+    }
 
     /**
      * 연관관계 편의 메서드
